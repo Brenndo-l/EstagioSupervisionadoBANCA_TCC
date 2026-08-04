c7162b32321fe2092be4562d55f5f49460c32d00
Alison <alison.soares@sou.ufac.br>
2026-08-03 22:12:24 -0500

    login com autenticação via django admin

--- bancasapp/urls.py
+++ bancasapp/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     # Caminho vazio '' significa a página principal (ex: localhost:8000/)
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     
     # Ex: localhost:8000/bancas/
     path('bancas/', views.visualizar_bancas, name='visualizar_bancas'),
     
     # Ex: localhost:8000/solicitar/
     path('solicitar/', views.solicitar_banca, name='solicitar_banca'),
+
+    path('', views.login_view, name="login")
 ]
